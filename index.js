59c49b687f1c2bfdc946665a5a713d60e136a291
Martijn Dekker - Kickoff Lab <martijn@kickofflab.nl>
2023-10-14 11:25:10 +0100

    Added screen management

--- index.js
+++ index.js
@@ -8,20 +8,41 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-let counter = 0;
+let state = {
+    currentScreen: 'screen1',
+    screens: {
+        screen1: {
+            message: "Hello, this is Screen 1!",
+            color: "blue"
+        },
+        screen2: {
+            message: "Welcome to Screen 2!",
+            color: "green"
+        },
+        screen3: {
+            message: "You're viewing Screen 3!",
+            color: "red"
+        }
+    }
+};
 
 io.on('connection', (socket) => {
     console.log('A user connected');
     
-    socket.emit('counterUpdate', counter);
+    socket.emit('stateUpdate', state);
     
-    socket.on('incrementCounter', () => {
-        counter++;
-        io.emit('counterUpdate', counter);
+    socket.on('changeState', (newState) => {
+        // Update global state and screen-specific state
+        state = { ...state, ...newState, screens: { ...state.screens, ...newState.screens } };
+        io.emit('stateUpdate', state);
     });
-    
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
+
+    socket.on('changeScreen', (screenName) => {
+        // Update the current screen if it exists in state.screens
+        if(state.screens[screenName]) {
+            state.currentScreen = screenName;
+            io.emit('stateUpdate', state);
+        }
     });
 });
 
@@ -32,3 +53,4 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
+
